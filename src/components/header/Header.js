e51101ca6f3a3ab440b357ee62f9e4e4c65e8d38
Zia Uddin <ziamaryam2526@gmail.com>
2025-11-14 04:54:16 -0800

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -17,7 +17,7 @@ import {
 function Header() {
   const {isDark} = useContext(StyleContext);
   const viewExperience = workExperiences.display;
-  const viewOpenSource = openSource.display;
+  const viewOpenSource = projects.display;
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
   const viewBlog = blogSection.display;
@@ -53,7 +53,7 @@ function Header() {
           )}
           {viewOpenSource && (
             <li>
-              <a href="#opensource">Open Source</a>
+              <a href="#Projects">Projects</a>
             </li>
           )}
           {viewAchievement && (
